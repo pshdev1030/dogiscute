be00ba375214602d9d0f67dc9e07aaeaf2153ffc
ParkSungHyun <psh1030.dev@gmail.com>
2021-08-27 21:16:30 +0900

    vanillajs에서 state를 이용한 컴포넌트관리

---
+++ Desktop/REACT/example/vanilla/tutorial/counter/src/core/Component.js
@@ -0,0 +1,35 @@
+export default class Component {
+  $target;
+  $state;
+  constructor($target) {
+    this.$target = $target;
+    this.setup();
+    this.setEvent();
+    this.render();
+  }
+
+  setup() {}
+  template() {
+    return "";
+  }
+  render() {
+    this.$target.innerHTML = this.template();
+  }
+  setEvent() {}
+  setState(newState) {
+    this.$state = { ...this.$state, ...newState };
+    this.render();
+  }
+
+  addEvent(eventType, selector, callback) {
+    const children = [...this.$target.querySelectorAll(selector)];
+
+    const istarget = (target) =>
+      children.includes(target) || target.closest(selector);
+
+    this.$target.addEventListener(eventType, (event) => {
+      if (!istarget(event.target)) return false;
+      callback(event);
+    });
+  }
+}
